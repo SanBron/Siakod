633513a4243d70742c2bdd7e708b7bae443221d6
SanBron <102327047+SanBron@users.noreply.github.com>
2022-03-24 23:57:22 +0300

    Add files via upload

---
+++ S.Vibor.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+using namespace std;
+
+void selectSort() {
+    const int n = 10;
+    int A[n] = {};
+    int min;
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        min = i;
+        for (int j = i + 1; j < n; j++) {
+            if (A[j] < A[min]) min = j;
+            if (i != min) {
+                swap(A[i], A[min]);
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+
+}
