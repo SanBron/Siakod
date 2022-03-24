cde6366da95fde5566d099a4aa085bf639816f21
SanBron <102327047+SanBron@users.noreply.github.com>
2022-03-24 23:49:15 +0300

    Add files via upload

---
+++ S.Shel.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int n = 10;
+    int A[n] = {};
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+    int d = n / 2;
+    int k, j;
+    while (d != 0) {
+        for (int i = 0; i < n - d; i++) {
+            j = i;
+            for (j = i; j >= 0; j--) {
+                if (A[j] > A[j + d]) {
+                    k = A[j];
+                    A[j] = A[j + d];
+                    A[j + d] = k;
+                    j--;
+                }
+            }
+        }
+        d = d / 2;
+    }
+    for (int i = 0; i < n; i++)
+        cout << A[i] << " ";
+    return 0;
+}
